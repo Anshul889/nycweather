4b014b4a5b62f9ef57cdb69882e6856f83db2f4d
Anshul889 <anshulmanaktala@gmail.com>
2017-07-05 08:31:02 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
       url: 'https://api.wunderground.com/api/4e1fe0d302980707/geolookup/conditions/q/NY/Manhattan.json',
       datatype: 'jsonp',
       success: function(data) {
-        $('.weather').html('<p class="data">' + data.current_observation.weather + '</p>');
+        $('.weather').html('<p class="data">' + data.current_observation.temperature_string + '</p>');
       }
     });
   });
